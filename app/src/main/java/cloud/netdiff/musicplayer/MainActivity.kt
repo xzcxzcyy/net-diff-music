071152beecf416e9552c7272fc8c2a530948fe06
Banson Xu <xzcxzcyy@126.com>
2020-07-25 20:25:53 +0800

    init commit

---
+++ app/src/main/java/cloud/netdiff/musicplayer/MainActivity.kt
@@ -0,0 +1,11 @@
+package cloud.netdiff.musicplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
