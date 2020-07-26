9f468c1f55629b025276310b91f72f9973c4a82b
Banson Xu <xzcxzcyy@126.com>
2020-07-26 09:46:53 +0800

    feat: dataBinding for ListFragment

--- app/src/main/java/cloud/netdiff/musicplayer/list/ListFragment.kt
+++ app/src/main/java/cloud/netdiff/musicplayer/list/ListFragment.kt
@@ -1,6 +1,5 @@
 package cloud.netdiff.musicplayer.list
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,15 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import cloud.netdiff.musicplayer.R
+import cloud.netdiff.musicplayer.databinding.ListFragmentBinding
+import java.util.zip.Inflater
 
 class ListFragment : Fragment() {
+
     private lateinit var viewModel: ListViewModel
+    private lateinit var dataBinding: ListFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.list_fragment, container, false)
+        dataBinding = ListFragmentBinding.inflate(inflater, container, false)
+        return dataBinding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
