2227d8cea7246c1a2b6655e27d934891b26550ba
bossjoker1 <1397157763@qq.com>
2020-07-26 22:29:57 +0800

    "try again"

--- app/src/main/java/cloud/netdiff/musicplayer/MainActivity.kt
+++ app/src/main/java/cloud/netdiff/musicplayer/MainActivity.kt
@@ -1,11 +1,85 @@
 package cloud.netdiff.musicplayer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var view1: View? = null
+    private var view2: View? = null
+    private lateinit var viewPager: ViewPager
+    private var viewList: List<View>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //去掉状态栏
+
+        //去掉状态栏
+        if (Build.VERSION.SDK_INT >= 21) {
+            val decorView = window.decorView
+            val option = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+            decorView.systemUiVisibility = option
+            window.navigationBarColor = Color.TRANSPARENT
+            window.statusBarColor = Color.TRANSPARENT
+        }
+
+        viewPager = findViewById(R.id.pager)
+        val inflater = layoutInflater
+        view1 = inflater.inflate(R.layout.title_local_fragment, null)
+        view2 = inflater.inflate(R.layout.title_online_fragment, null)
+
+        viewList = ArrayList()
+
+        (viewList as ArrayList<View>).add(view1!!)
+        (viewList as ArrayList<View>).add(view2!!)
+
+        //传递viewList出了点问题，先暂且直接放在activity下
+
+        var pagerAdapter: PagerAdapter = object : PagerAdapter() {
+            override fun isViewFromObject(arg0: View, arg1: Any): Boolean {
+                return arg0 == arg1
+            }
+
+            override fun getCount(): Int {
+                return viewList!!.size
+            }
+
+            override fun destroyItem(
+                container: ViewGroup, position: Int,
+                `object`: Any
+            ) {
+                container.removeView(viewList!![position])
+            }
+
+            override fun instantiateItem(container: ViewGroup, position: Int): Any {
+                container.addView(viewList!![position])
+                return viewList!![position]
+            }
+
+            override fun getPageTitle(position: Int): CharSequence? {
+                return when (position) {
+                    0 -> "本地音乐"
+                    1 -> "在线搜索"
+                    else -> "Title default"
+                }
+            }
+
+
+        }
+
+        viewPager.adapter = pagerAdapter
+
     }
+
+
 }
--- app/src/main/java/cloud/netdiff/musicplayer/title/TitleFragment.kt
+++ app/src/main/java/cloud/netdiff/musicplayer/title/TitleFragment.kt
@@ -1,12 +1,14 @@
 package cloud.netdiff.musicplayer.title
 
-import androidx.lifecycle.ViewModelProviders
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import cloud.netdiff.musicplayer.R
+
 
 class TitleFragment : Fragment() {
 
@@ -25,8 +27,7 @@ class TitleFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(TitleViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this).get(TitleViewModel::class.java)
     }
 
 }
