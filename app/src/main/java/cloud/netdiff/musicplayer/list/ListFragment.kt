47314b3fe03649814b9f904aa55c8c17e3cd295a
Banson Xu <xzcxzcyy@126.com>
2020-07-26 09:51:51 +0800

    fix: data binding for ListFragment.kt

--- app/src/main/java/cloud/netdiff/musicplayer/list/ListFragment.kt
+++ app/src/main/java/cloud/netdiff/musicplayer/list/ListFragment.kt
@@ -5,10 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import cloud.netdiff.musicplayer.R
 import cloud.netdiff.musicplayer.databinding.ListFragmentBinding
-import java.util.zip.Inflater
 
 class ListFragment : Fragment() {
 
@@ -19,7 +19,7 @@ class ListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dataBinding = ListFragmentBinding.inflate(inflater, container, false)
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.list_fragment, container, false)
         return dataBinding.root
     }
 
