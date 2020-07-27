dfdf0da21452af7be836f8391448f40b7de09ad6
Banson Xu <xzcxzcyy@126.com>
2020-07-27 09:36:08 +0800

    fix: recyclerview

--- app/src/main/java/cloud/netdiff/musicplayer/list/ListFragment.kt
+++ app/src/main/java/cloud/netdiff/musicplayer/list/ListFragment.kt
@@ -1,6 +1,7 @@
 package cloud.netdiff.musicplayer.list
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,9 +10,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import cloud.netdiff.musicplayer.R
+import cloud.netdiff.musicplayer.database.Song
 import cloud.netdiff.musicplayer.databinding.ListFragmentBinding
 
 class ListFragment : Fragment() {
+
+    private val TAG = "ListFragment"
 
     private lateinit var viewModel: ListViewModel
     private lateinit var dataBinding: ListFragmentBinding
@@ -27,7 +31,21 @@ class ListFragment : Fragment() {
 
         //TODO: get list name out of database, and change the toolbar title
 
+        val adapter = SongAdapter(SongListener {
+            Log.d(TAG, "SongAdapter: item click event listened.")
+        })
+        dataBinding.listSongRecycler.adapter = adapter
 
+        //TODO: Only for test here.
+        val testList = mutableListOf<Song>()
+        var i = 1
+        while (i <= 100) {
+            val t = Song(songId = i.toLong(), songName = i.toString())
+            testList.add(t)
+            i += 1
+        }
+        Log.d(TAG, "onCreateView: ${testList.toString()}")
+        adapter.submitList(testList.toList())
 
         return dataBinding.root
     }
